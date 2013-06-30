700e891e06fa4528a09e1bdf488b23c053abad22
UG-Moustafa <UG-Moustafa@UG-Moustafa-PC>
2013-06-30 03:25:44 +0200

    firs commit

---
+++ src/gethup/test1/GetHupTest1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gethup.test1;
+
+/**
+ *
+ * @author UG-Moustafa
+ */
+public class GetHupTest1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" moustafa 1");
+        System.out.println(" moustafa 1");
+        System.out.println(" moustafa 1");
+        System.out.println(" moustafa 1");
+        System.out.println(" moustafa 1");
+    }
+}
